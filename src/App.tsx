187df6026070e767c61f4ba4d48f9fc4b620db98
Adewale Nurudeen <nurudeenadeolaa@gmail.com>
2021-02-08 07:44:49 +0100

    gh-pages settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.scss';
 
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import Home from './components/Routes/Home';
 import About from './components/Routes/About';
 import Contact from './components/Routes/Contact';
